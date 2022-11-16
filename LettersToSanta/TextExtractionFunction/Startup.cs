14f5522ec49618182bb121c0489c42397ee68c0a
Sam Gomez <Sam.Gomez@geneca.com>
2022-11-15 19:09:07 -0500

    Added Azure function project.

---
+++ LettersToSanta/TextExtractionFunction/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(TextExtractionFunction.Startup))]
+
+namespace TextExtractionFunction
+{
+    class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string cs = Environment.GetEnvironmentVariable("ConnectionString");
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(cs);
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
